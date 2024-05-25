9c5e9d486b57a1f8853606be7d4fced984242406
patterni <marchenkovpasha@gmail.com>
2024-05-25 20:24:56 +0300

    back up

--- script.js
+++ script.js
@@ -40,34 +40,3 @@ window.addEventListener('scroll', () => {
             navLinks.classList.toggle('show');
             brand.classList.toggle('hidden');
         });
-
-        
-document.addEventListener("DOMContentLoaded", function() {
-    var div, n,
-        v = document.getElementsByClassName("youtube-player");
-    for (n = 0; n < v.length; n++) {
-        div = document.createElement("div");
-        div.setAttribute("data-id", v[n].dataset.id);
-        div.innerHTML = labnolThumb(v[n].dataset.id);
-        div.onclick = labnolIframe;
-        v[n].appendChild(div);
-    }
-});
-
-function labnolThumb(id) {
-    var thumb = '<img src="https://i.ytimg.com/vi/ID/hqdefault.jpg">',
-        play = '<div class="play-button"></div>';
-    return thumb.replace("ID", id) + play;
-}
-
-function labnolIframe() {
-    var iframe = document.createElement("iframe");
-    var embed = "https://www.youtube.com/embed/ID?autoplay=1";
-    iframe.setAttribute("src", embed.replace("ID", this.dataset.id));
-    iframe.setAttribute("frameborder", "0");
-    iframe.setAttribute("allowfullscreen", "1");
-    iframe.setAttribute("allow", "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share");
-    iframe.setAttribute("width", "560"); // Додати ширину
-    iframe.setAttribute("height", "315"); // Додати висоту
-    this.parentNode.replaceChild(iframe, this);
-}
